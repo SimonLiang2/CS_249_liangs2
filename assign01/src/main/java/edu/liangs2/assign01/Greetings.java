da3ee8a66d8a693bb3b90a48a052e3b87e0fc4d2
simonliang4 <liangs2@sunypoly.edu>
2023-08-31 14:27:11 -0400

    Finished code

---
+++ assign01/src/main/java/edu/liangs2/assign01/Greetings.java
@@ -0,0 +1,8 @@
+package edu.liangs2.assign01;
+
+public class Greetings {
+    public static void main(String [] args){
+
+        System.out.println("Hello World");
+    }
+}
--- assign01/src/main/java/module-info.java
+++ assign01/src/main/java/module-info.java
@@ -1,3 +1,3 @@
 module assign01mod {    
-    exports edu.realemj.assign01; 
+    exports edu.liangs2.assign01;
 }
