227569dcff0a74c4900929956fd22020db308be5
simonliang4 <liangs2@sunypoly.edu>
2023-10-02 13:07:41 -0400

    Assignment 2 and Screenshot completed

---
+++ exercises/src/main/java/edu/liangs2/exercises09/MatrixLand.java
@@ -0,0 +1,13 @@
+package edu.liangs2.exercises09;
+import edu.liangs2.exercises09.Matrix;
+
+public class MatrixLand {
+    public static void main(String [] args){
+        Matrix p = Matrix.mainPointID(5,3,-2);
+        System.out.println(p);
+        Matrix t = Matrix.makeTranslation3D(2,8,0);
+        System.out.println(t);
+        Matrix p2 = t.multiply(p);
+        System.out.println(p2);
+    }
+}
--- exercises/src/main/java/module-info.java
+++ exercises/src/main/java/module-info.java
@@ -3,4 +3,5 @@ module exercisesmod {
     exports edu.liangs2.exercises03;
     exports edu.liangs2.exercises06;
     exports edu.liangs2.exercises07;
+    exports edu.liangs2.exercises09;
 }
