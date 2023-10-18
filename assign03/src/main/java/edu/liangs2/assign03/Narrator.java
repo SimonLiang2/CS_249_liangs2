0ba88a0c4fddb3c3bccd5d9dce44037ff1683262
simonliang4 <liangs2@sunypoly.edu>
2023-10-18 01:10:07 -0400

    Started on assignment 3 Added the functions Some functions are WIP

--- assign03/src/main/java/edu/liangs2/assign03/Narrator.java
+++ assign03/src/main/java/edu/liangs2/assign03/Narrator.java
@@ -1,4 +1,47 @@
 package edu.liangs2.assign03;
 
+
 public class Narrator {
+    char boundaryChar;
+    public Narrator(String [] lines,char boundaryChar){
+        //Need to implement stuff/functions for it to do its magic
+        //TODO
+    }
+
+    public char getBoundaryChar(){
+        return boundaryChar;
+    }
+
+    public String getLines(){
+        //TODO
+        return "";
+    }
+
+    public void setBoundaryChar(char boundaryChar){
+        this.boundaryChar = boundaryChar;
+    }
+
+    public void setLines(String [] lines){
+        //TODO
+    }
+    public String generateBoundaryLine(){
+        StringBuilder p = new StringBuilder();
+        for(int i =0; i<50; i++) {
+            p.append(getBoundaryChar());
+        }
+        return p.toString();
+    }
+
+    public String generateCenteredLine(String text){
+        //TODO
+        return "";
+    }
+
+    public String toString(){
+        //TODO
+        return "";
+    }
+
+
+
 }
--- assign03/src/main/java/edu/liangs2/assign03/SilentFilm.java
+++ assign03/src/main/java/edu/liangs2/assign03/SilentFilm.java
@@ -1,4 +1,35 @@
 package edu.liangs2.assign03;
+import java.util.*;
 
 public class SilentFilm {
+    public static Narrator askforNarration(Scanner input){
+
+        String line;
+        char boundaryChar;
+        int numLines;
+
+        System.out.println("Enter boundary character:");
+        boundaryChar = input.nextLine().charAt(0);
+        System.out.println("Enter the number of lines:");
+        line = input.nextLine();
+        numLines = Integer.parseInt(line);
+        String [] allLines = new String [numLines];
+        System.out.println("Enter lines:");
+        for (int i=0; i < numLines; i++){
+            line = input.nextLine();
+            allLines[i] = line;
+        }
+        //might change n where you can add the number of lines in
+        //Narrator n = new Narrator(allLines, boundaryChar, numLines);
+        //TODO
+        Narrator n = new Narrator(allLines, boundaryChar);
+        return n;
+    }
+
+    public static void main(String [] args){
+        Scanner input = new Scanner(System.in);
+        Narrator n = askforNarration(input);
+        System.out.println("Our story unfortunately begins...");
+        System.out.println(n);
+    }
 }
