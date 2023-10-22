d4acdc692c967a7d05a545993bd5e11850eab759
simonliang4 <liangs2@sunypoly.edu>
2023-10-22 07:14:26 -0400

    Finished toString and rest of code + screenshot

--- assign03/src/main/java/edu/liangs2/assign03/Narrator.java
+++ assign03/src/main/java/edu/liangs2/assign03/Narrator.java
@@ -1,11 +1,24 @@
 package edu.liangs2.assign03;
+import java.util.*;
+
 
 
 public class Narrator {
     char boundaryChar;
+    int numLines;
+
+    int MAXLINES=4;
+
+    String [] lines;
+
+
+
     public Narrator(String [] lines,char boundaryChar){
         //Need to implement stuff/functions for it to do its magic
         //TODO
+        setBoundaryChar(boundaryChar);
+        setLines(lines);
+
     }
 
     public char getBoundaryChar(){
@@ -14,7 +27,12 @@ public class Narrator {
 
     public String getLines(){
         //TODO
-        return "";
+        StringBuilder build = new StringBuilder();
+        for (int i =0; i < numLines;i++){
+            build.append(lines[i]);
+            build.append("\n");
+        }
+        return build.toString();
     }
 
     public void setBoundaryChar(char boundaryChar){
@@ -23,25 +41,85 @@ public class Narrator {
 
     public void setLines(String [] lines){
         //TODO
+        numLines = lines.length;
+        this.lines = new String[numLines];
+        for (int i = 0; i < numLines; i++) {
+            if (lines[i]==null){
+                this.lines[i] = "";
+            }
+            else {
+                this.lines[i] = lines[i];
+            }
+        }
     }
     public String generateBoundaryLine(){
         StringBuilder p = new StringBuilder();
         for(int i =0; i<50; i++) {
             p.append(getBoundaryChar());
         }
+        p.append("\n");
         return p.toString();
     }
 
     public String generateCenteredLine(String text){
         //TODO
-        return "";
+        int totalSpace = (50 - text.length()) -2;
+        int space1= totalSpace/2;
+        int space2= totalSpace - space1;
+
+        StringBuilder centerLine = new StringBuilder();
+        centerLine.append(getBoundaryChar());
+        for (int i =0; i <space1; i++) {
+            centerLine.append(' ');
+        }
+        centerLine.append(text);
+        for(int i=0; i<space2; i++) {
+            centerLine.append(' ');
+        }
+        centerLine.append(getBoundaryChar());
+        centerLine.append("\n");
+        return centerLine.toString();
     }
 
     public String toString(){
+
         //TODO
-        return "";
+        if(numLines == 0){
+            return "";
+        }
+
+        StringBuilder last = new StringBuilder();
+
+        int count =0;
+
+        int fullCards = numLines/MAXLINES;
+        int lastCardLines = numLines % MAXLINES;
+
+        for (int i = 1; i <= fullCards;i++){
+            last.append(generateBoundaryLine());
+            last.append(generateCenteredLine(""));
+            for(int j = 1; j <= MAXLINES; j++){
+                last.append(generateCenteredLine(lines[count]));
+                count++;
+            }
+            last.append(generateCenteredLine(""));
+            last.append(generateBoundaryLine());
+        }
+
+
+        if(lastCardLines!=0) {
+            last.append(generateBoundaryLine());
+            last.append(generateCenteredLine(""));
+            for (int i = 1; i <= lastCardLines; i++) {
+                last.append(generateCenteredLine(lines[(count)]));
+                count++;
+            }
+            for (int i = 0; i < (MAXLINES - lastCardLines); i++) {
+                last.append(generateCenteredLine(""));
+            }
+            last.append(generateCenteredLine(""));
+            last.append(generateBoundaryLine());
+        }
+        return last.toString();
     }
-
-
-
 }
--- assign03/src/main/java/edu/liangs2/assign03/SilentFilm.java
+++ assign03/src/main/java/edu/liangs2/assign03/SilentFilm.java
@@ -2,34 +2,41 @@ package edu.liangs2.assign03;
 import java.util.*;
 
 public class SilentFilm {
-    public static Narrator askforNarration(Scanner input){
+    public static Narrator askForNarration(Scanner input){
 
         String line;
         char boundaryChar;
         int numLines;
+        String [] allLines;
 
         System.out.println("Enter boundary character:");
         boundaryChar = input.nextLine().charAt(0);
-        System.out.println("Enter the number of lines:");
+        System.out.println("Enter number of lines:");
         line = input.nextLine();
         numLines = Integer.parseInt(line);
-        String [] allLines = new String [numLines];
+
+
         System.out.println("Enter lines:");
-        for (int i=0; i < numLines; i++){
-            line = input.nextLine();
-            allLines[i] = line;
+        if (numLines > 0) {
+            allLines = new String[numLines];
+            for (int i = 0; i < numLines; i++) {
+                line = input.nextLine();
+                allLines[i] = line;
+            }
         }
-        //might change n where you can add the number of lines in
-        //Narrator n = new Narrator(allLines, boundaryChar, numLines);
-        //TODO
+        else{
+            allLines = new String[0];
+        }
+
+
         Narrator n = new Narrator(allLines, boundaryChar);
         return n;
     }
 
     public static void main(String [] args){
         Scanner input = new Scanner(System.in);
-        Narrator n = askforNarration(input);
-        System.out.println("Our story unfortunately begins...");
+        Narrator n = askForNarration(input);
+        System.out.println("Our story begins...");
         System.out.println(n);
     }
 }
