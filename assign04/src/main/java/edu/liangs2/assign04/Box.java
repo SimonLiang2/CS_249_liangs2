6fb3a771cbf4d26acb1a28625270954450ea7d81
simonliang4 <liangs2@sunypoly.edu>
2023-11-06 21:46:59 -0500

    Finished Assignment 4 Code; Need to work on UML Diagram

--- assign04/src/main/java/edu/liangs2/assign04/Box.java
+++ assign04/src/main/java/edu/liangs2/assign04/Box.java
@@ -1,4 +1,55 @@
 package edu.liangs2.assign04;
 
 public class Box {
+
+    private int startRow = 0;
+    private int startCol = 0;
+    private int endRow = 0;
+    private int endCol = 0;
+    private boolean isFilled = false;
+
+    private char drawChar = ' ';
+
+    public Box(int sr, int sc, int er,int ec, boolean filled, char drawChar){
+        startRow = sr;
+        startCol = sc;
+        endRow = er;
+        endCol = ec;
+        isFilled = filled;
+        this.drawChar = drawChar;
+    }
+
+    public String toString(){
+        if(isFilled){
+            return "Filled Box from (" + startRow + "," + startCol + ") to ("
+                    + endRow + "," + endCol + ") with char '" + drawChar + "'";
+        }
+        else{
+            return "Box from (" + startRow + "," + startCol + ") to ("
+                    + endRow + "," + endCol + ") with char '" + drawChar + "'";
+        }
+    }
+
+    public void draw(ScreenBuffer s) {
+        if (isFilled) {
+            for (int i = startRow; i <= endRow; i++) {
+                for (int j = startCol; j <= endCol; j++) {
+                    s.setPos(i, j, drawChar);
+                }
+            }
+        }
+        else{
+
+                for (int i = startRow; i <= endRow; i++) {
+                    s.setPos(i,startCol,drawChar);
+                    s.setPos(i,endCol,drawChar);
+                }
+                for (int j = startCol; j <= endCol; j++){
+                    s.setPos(startRow,j,drawChar);
+                    s.setPos(endRow,j,drawChar);
+                }
+
+
+        }
+    }
 }
--- assign04/src/main/java/edu/liangs2/assign04/ScreenBuffer.java
+++ assign04/src/main/java/edu/liangs2/assign04/ScreenBuffer.java
@@ -1,4 +1,81 @@
 package edu.liangs2.assign04;
 
 public class ScreenBuffer {
+
+    private char [][] array = new char [1][1];
+    private int rowCnt = 0;
+    private int colCnt = 0;
+
+    private char fillChar = ' ';
+
+    public ScreenBuffer(int rowCnt, int colCnt, char fillChar){
+        this.fillChar= fillChar;
+        this.rowCnt = rowCnt;
+        this.colCnt= colCnt;
+        array = new char [rowCnt][colCnt];
+        for (int i =0; i<rowCnt; i++){
+            for (int j=0; j<colCnt; j++){
+                array[i][j] = fillChar;
+            }
+        }
+    }
+
+    public void clear(){
+        for (int i =0; i<rowCnt; i++){
+            for (int j=0; j<colCnt; j++){
+                array[i][j] = fillChar;
+            }
+        }
+    }
+
+    public int getRowCnt(){
+        return rowCnt;
+    }
+    public int getColCnt(){
+        return colCnt;
+    }
+
+    public boolean isValidPosition(int row, int col){
+        if(((row >=0) && (row < rowCnt)) && ((col >=0) && (col < colCnt))){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public char getPos(int row, int col){
+        if (isValidPosition(row,col)){
+            return array [row][col];
+        }
+        else{
+            return ' ';
+        }
+    }
+
+    public boolean setPos(int row, int col, char c){
+        if(isValidPosition(row,col)){
+            array [row][col]= c;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public String toString(){
+        return rowCnt + " x " + colCnt + " ScreenBuffer (default: " + fillChar + ")";
+    }
+
+    public String getDisplayString(){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < rowCnt; i++){
+            for (int j = 0; j < colCnt; j++){
+                sb.append(array[i][j]);
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
 }
